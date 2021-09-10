3fa6d9ec604fc8be735be3bb29fdd349950d0d44
Christian Rocha <christian@rocha.is>
2021-09-10 12:39:35 -0400

    Add network/disk/cpu/uptime monitors

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,8 @@
 module Main where
 
 import Xmobar
-    ( Config(..), Date(Date), Monitors(Wireless, Memory), Runnable(Run)
+    ( Config(..), Date(Date), Monitors(Wireless, Memory, Uptime, Cpu, Network, DiskU), Runnable(Run)
     , StdinReader(StdinReader), XPosition(BottomP), defaultConfig, xmobar )
-
 config :: Config
 config =
     defaultConfig
@@ -15,11 +14,15 @@ config =
     , commands = [ Run $ Date "星期%u %Y年%m月%d日 %H:%M" "date" 600
                  , Run $ Wireless "wlp76s0" [ "-x", "", "-t", "<essid>" ] 100
                  , Run StdinReader
+                 , Run $ Network "enp78s0" [ "-t", "↓<rx>KB ↑<tx>KB" ] 10
+                 , Run $ DiskU [ ("/", "/: <used>/<size>") ] [] 60
+                 , Run $ Cpu ["-L","5","-H","50","--normal","green","--high","red", "-t", "CPU: <total>%"] 10
+                 , Run $ Uptime ["-t", "Up: <days>d <hours>h <minutes>m"] 36000
                  , Run $ Memory [ "-t", "Mem: <usedratio>%" ] 10
                  ]
     , sepChar  = "%"
     , alignSep = "}{"
-    , template = " %StdinReader% } <fc=#505050>%date%</fc> { %memory% "
+    , template = " %StdinReader% } <fc=#505050>%date%</fc> { %enp78s0%   %disku%   %cpu%   %uptime%   %memory% "
     }
 
 main :: IO ()
